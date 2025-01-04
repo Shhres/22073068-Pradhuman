3d26af61ab74e1abafabbfc62d6d620c3bba565e
Shhres <pradhuman10.x@gmail.com>
2025-01-04 10:16:44 +0545

    12 commit

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c5e9aae8b53300ea48e2e8c3dd66dcf5302582fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+5ee67cdbb427787fd683e8cb4b241c24e958cc6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
