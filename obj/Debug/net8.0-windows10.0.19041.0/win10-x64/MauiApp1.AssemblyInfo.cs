4be66c63e21afcd9b80e26af39e43b939e881208
Shhres <pradhuman10.x@gmail.com>
2025-01-14 17:19:29 +0545

    dashboard debit list

--- obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows10.0.19041.0/win10-x64/MauiApp1.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+493ae9e1c5d8e1aa761a46be3bbcab45fda0c7f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+da3f242d1e6c9c83b974653e6219efaf98da764d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
